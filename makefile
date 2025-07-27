153117e23553c596dea25f3843e3902088b5a28c
MosElAgab <mostafa.elagab@gmail.com>
2025-07-28 00:10:55 +0100

    sets up makefile

---
+++ makefile
@@ -0,0 +1,21 @@
+# Project configuration
+PROJECT_NAME = multinational-retail-data-sources-setup
+
+# Defined targets
+.PHONY: tarraform-init terraform-plan terraform-apply terraform-destroy
+
+# 
+terraform-init:
+	terraform -chdir=terraform init
+
+# 
+terraform-plan:
+	terraform -chdir=terraform plan
+
+# 
+terraform-apply:
+	terraform -chdir=terraform apply
+
+# 
+terraform-destroy:
+	terraform -chdir=terraform destroy
---
+++ src.py
@@ -0,0 +1,50 @@
+from sqlalchemy import create_engine, URL
+import pandas as pd
+
+
+RDS_USER = "retail_admin"
+RDS_PASSWORD = "retail_admin_1234"
+RDS_HOST= "multinational-retail.c7kegm862wks.eu-west-2.rds.amazonaws.com"
+RDS_DATABASE="postgres"
+RDS_PORT="5432"
+
+def init_db_engine():
+    """
+    Initialize a SQLAlchemy database engine for AWS RDS based on provided credentials.
+
+    Returns:
+        Engine: SQLAlchemy engine object.
+    """
+    DATABASE_TYPE = "postgresql"
+    DBAPI = "psycopg2"
+    url_object = URL.create(
+        f"{DATABASE_TYPE}+{DBAPI}",
+        username=RDS_USER,
+        password=RDS_PASSWORD,
+        host=RDS_HOST,
+        database=RDS_DATABASE,
+        port=RDS_PORT
+    )
+    engine = create_engine(url_object)
+    return engine
+
+csv_file = "./data/legacy_users.csv"
+
+df = pd.read_csv(csv_file, index_col=False)
+df.drop(columns=["Unnamed: 0"], inplace=True)
+
+# df.drop(columns="index", inplace=True)
+
+df.info()
+
+engine = init_db_engine()
+table_name = "legacy_users"
+
+df.to_sql(
+            name=table_name,
+            con=engine,
+            if_exists="replace",
+            index=False
+        )
+
+# orders with rds as well
