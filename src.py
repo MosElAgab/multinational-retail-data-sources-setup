8429024363c6f00d6a3d6af1409b28f3714a9af7
MosElAgab <mostafa.elagab@gmail.com>
2025-07-28 14:13:57 +0100

    refactors code

--- src.py
+++ src.py
@@ -5,49 +5,50 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-RDS_USER = os.getenv("RDS_USER")
-RDS_PASSWORD = os.getenv("RDS_PASSWORD")
-RDS_HOST = os.getenv("RDS_HOST")
-RDS_DATABASE = os.getenv("RDS_DATABASE")
-RDS_PORT = os.getenv("RDS_PORT")
 
-def init_db_engine():
-    """
-    Initialize a SQLAlchemy database engine for AWS RDS based on provided credentials.
+class DatabaseConnector():
+    def __init__(self):
+        pass
 
-    Returns:
-        Engine: SQLAlchemy engine object.
-    """
-    DATABASE_TYPE = "postgresql"
-    DBAPI = "psycopg2"
-    url_object = URL.create(
-        f"{DATABASE_TYPE}+{DBAPI}",
-        username=RDS_USER,
-        password=RDS_PASSWORD,
-        host=RDS_HOST,
-        database=RDS_DATABASE,
-        port=RDS_PORT
-    )
-    engine = create_engine(url_object)
-    return engine
+    def init_rds_db_engine(self):
+        """
+        Initialize a SQLAlchemy database engine for AWS RDS based on provided credentials.
+
+        Returns:
+            Engine: SQLAlchemy engine object.
+        """
+        RDS_USER = os.getenv("RDS_USER")
+        RDS_PASSWORD = os.getenv("RDS_PASSWORD")
+        RDS_HOST = os.getenv("RDS_HOST")
+        RDS_DATABASE = os.getenv("RDS_DATABASE")
+        RDS_PORT = os.getenv("RDS_PORT")
+        DATABASE_TYPE = "postgresql"
+        DBAPI = "psycopg2"
+    
+        url_object = URL.create(
+            f"{DATABASE_TYPE}+{DBAPI}",
+            username=RDS_USER,
+            password=RDS_PASSWORD,
+            host=RDS_HOST,
+            database=RDS_DATABASE,
+            port=RDS_PORT
+        )
+        engine = create_engine(url_object)
+        return engine
+
+connecter = DatabaseConnector()
 
 csv_file = "./data/legacy_users.csv"
-
 df = pd.read_csv(csv_file, index_col=False)
 df.drop(columns=["Unnamed: 0"], inplace=True)
-
-# df.drop(columns="index", inplace=True)
-
 df.info()
 
-engine = init_db_engine()
+engine = connecter.init_rds_db_engine()
 table_name = "legacy_users"
-
 df.to_sql(
             name=table_name,
             con=engine,
             if_exists="replace",
             index=False
         )
-
-# orders with rds as well
+print(f"Uploading {df.shape[0]} records to RDS table '{table_name}'...")
