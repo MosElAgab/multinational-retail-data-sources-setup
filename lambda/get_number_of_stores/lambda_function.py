23dca592e8e05f5b850a6f430f15b55b3324c918
MosElAgab <mostafa.elagab@gmail.com>
2025-08-05 13:13:34 +0100

    adds missing env variable error handling get_number_of_store_lambda_function

--- lambda/get_number_of_stores/lambda_function.py
+++ lambda/get_number_of_stores/lambda_function.py
@@ -1,5 +1,21 @@
 import boto3
+import os
+import json
+from dotenv import load_dotenv
 
+# load env for local development
+# load_dotenv()
 
 def lambda_handler(event, context):
-    pass
+    bucket_name = os.getenv("BUCKET_NAME")
+    object_name = os.getenv("STORE_CSV_OBJECT_KEY")
+    
+    if not bucket_name or not object_name:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"message": "missing env variables: BUCKET_NAME or TORE_CSV_OBJECT_KEY"})
+        }
+    
+# test
+response = lambda_handler({}, {})
+print(response)
