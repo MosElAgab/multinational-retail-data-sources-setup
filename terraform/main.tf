98142fc29a676b0d5cc28a877e85464c6e0feb8f
MosElAgab <mostafa.elagab@gmail.com>
2025-08-06 16:05:02 +0100

    adds aws_api_gateway_integration and aws_lambda_permission in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -147,3 +147,23 @@ resource "aws_api_gateway_api_key" "store_api_key" {
   value = var.store_api_key_value
   enabled = true
 }
+
+## intergrate api with lambda
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id = aws_api_gateway_rest_api.store_api.id
+  resource_id = aws_api_gateway_resource.number_of_stores.id
+  http_method = aws_api_gateway_method.get_number_of_stores.http_method
+  integration_http_method = "POST"
+  type = "AWS_PROXY"
+  uri = aws_lambda_function.get_number_of_stores.invoke_arn
+}
+
+
+## lambda invoke permission for the api integration
+resource "aws_lambda_permission" "allow_api_gateway_invoke" {
+  statement_id = "AllowExecutionFromAPIGateway"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.get_number_of_stores.function_name
+  principal = "apigateway.amazonaws.com"
+  source_arn = "${aws_api_gateway_rest_api.store_api.execution_arn}/*/*"
+}
