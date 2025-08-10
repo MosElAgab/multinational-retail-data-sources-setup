2e7e88ba273222caf6b464a5bc8d445ff90b4602
MosElAgab <mostafa.elagab@gmail.com>
2025-08-10 14:32:33 +0100

    adds get_store_data to lambda.get_store.lambda_function

--- lambda/get_store/lambda_function.py
+++ lambda/get_store/lambda_function.py
@@ -1,10 +1,21 @@
 from dotenv import load_dotenv
 import json
 import os
+import boto3
+import csv
+import io
 
 # load env for local development
 load_dotenv()
 
+def get_store_data(bucket_name, object_key):
+    # should retrun list of dictionaries given csv file
+    s3_client = boto3.client('s3')
+    response = s3_client.get_object(Bucket=bucket_name, Key=object_key)
+    content = response['Body'].read().decode('utf-8')
+    csv_reader = csv.DictReader(io.StringIO(content))
+    data = list(csv_reader)
+    return data
 
 
 def lambda_handler(event, context):
--- lambda/get_store/test_get_store_lambda_function.py
+++ lambda/get_store/test_get_store_lambda_function.py
@@ -1,6 +1,6 @@
 import json
-from get_store.lambda_function import lambda_handler
-
+from get_store.lambda_function import lambda_handler, get_store_data
+from unittest.mock import patch, MagicMock
 
 def test_lambda_handler_missing_env(monkeypatch):
     # delete env variables
@@ -14,3 +14,17 @@ def test_lambda_handler_missing_env(monkeypatch):
     # assert
     assert response["statusCode"] == 500
     assert "missing env" in body["message"]
+
+
+@patch("get_store.lambda_function.boto3.client")
+def test_get_store_data(mock_boto3_client):
+    mock_csv = "id,store_name\n1,Store A\n2,Store B\n3, Store b"
+    mock_response = {
+        'Body': MagicMock(read=MagicMock(return_value=mock_csv.encode("utf-8")))
+    }
+    mock_s3 = MagicMock()
+    mock_s3.get_object.return_value = mock_response
+    mock_boto3_client.return_value = mock_s3
+    
+    data = get_store_data("fake-bucket", "fake-key.csv")
+    assert isinstance(data, list)
